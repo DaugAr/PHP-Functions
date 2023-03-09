db0393fb9abcc6beceab2b1c0e22013e0074f5ab
DaugAr <arnasdaugela@gmail.com>
2023-03-09 18:56:36 +0200

    PHP Funkcijos ND 1-4

--- index.php
+++ index.php
@@ -26,10 +26,40 @@
 
     function Vardas($vard)
     {
-        echo "Labas, $vard";
+        echo "Labas, $vard!";
     }
 
-    vardas('Linai');
+    vardas("Linai");
+
+    echo "<hr>";
+    echo "Funkcijos ND 3";
+    echo "<br>";
+
+    function ZodzioIlgis($ilgis)
+    {
+        echo strlen($ilgis);
+    }
+
+    ZodzioIlgis("Naktis");
+
+    echo "<hr>";
+    echo "Funkcijos ND 4";
+    echo "<br>";
+
+    function Inicialai($vardas, $pavarde)
+    {
+        $initials = strtoupper(substr($vardas, 0, 1)) . strtoupper(substr($pavarde, 0, 1));
+        return $initials;
+    }
+
+    $name = "Arnas";
+    $surname = "Daugela";
+    $initials = Inicialai($name, $surname);
+    echo $initials;
+
+    echo "<hr>";
+    echo "Funkcijos ND 5";
+    echo "<br>";
 
     ?>
 
